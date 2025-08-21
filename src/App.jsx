4395b6f06a8df870e5d80093e6629e340bf3ea62
dewi-xaltius <smdewi@xaltius.tech>
2025-08-21 16:45:03 +0800

    Remove linting error and MyName component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css';
 import ProductPage from './components/ProductPage';
-import MyName from './components/MyName';
 
 function App() {
-  const unusedVariable = 'This variable is not used anywhere in the code';
   return (
     <>
-      <MyName />
       <ProductPage />
     </>
   )
