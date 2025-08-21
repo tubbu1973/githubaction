fb9e25bcdad3833d839ba9567e4e11b8fd400c3e
dewi-xaltius <smdewi@xaltius.tech>
2025-08-21 16:41:22 +0800

    Lint error to test CI workflow

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ProductPage from './components/ProductPage';
 import MyName from './components/MyName';
 
 function App() {
-  
+  const unusedVariable = 'This variable is not used anywhere in the code';
   return (
     <>
       <MyName />
